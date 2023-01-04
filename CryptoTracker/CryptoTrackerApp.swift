7f7a4a5d79a36103c5c0ed91e7c26a27bf0e89d8
GongCode <jasonxgong@gmail.com>
2023-01-04 01:15:08 -0600

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Jason Gong on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
