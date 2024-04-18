8e18991393ba645b8f9409bb2a4e192f78d892fa
JasonRedbird <jason.gong@redbird.io>
2024-04-18 18:58:53 -0400

    Add

---
+++ CryptoTracker/Extensions/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  CryptoTracker
+//
+//  Created by Jason Gong on 1/4/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let primaryTextColor = Color("PrimaryTextColor")
+    let itemBackgroundColor = Color("itemBackgroundColor")
+
+}
